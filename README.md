30713e4cc5be35202f51122f1cacba8a540adf04
tobias <tobias.omondi@student.moringaschool.com>
2023-10-03 18:09:15 +0300

    updated work

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # Phase-3-SQLAlchemy-Code
+
+For this assignment, we'll be working with a restaurant review domain. We have three models: `Restaurant`, `Review`, and `Customer`.
+For our purposes, a Restaurant  has many `Review`s, a `Customer` has many `Review`s, and a `Review` belongs to a `Restaurant` and to a `Customer`. `Restaurant` - `Customer` is a many-to-many relationship.
+**Note**: You should draw your domain on paper or on a whiteboard _before you start coding_. Remember to identify a single source of truth for your data.
+
+Use SQLAlchemy query methods where appropriate.
+Review
+- `Review customer()`
+  - should return the `Customer` instance for this review
+- `Review restaurant()`
+  - should return the `Restaurant` instance for this review
+ 
+
+Restaurant
+- `Restaurant reviews()`
+  - returns a collection of all the reviews for the `Restaurant`
+- `Restaurant customers()`
+  - returns a collection of all the customers who reviewed the `Restaurant`
+ 
+
+Customer
+- `Customer reviews()`
+  - should return a collection of all the reviews that the `Customer` has left
+- `Customer restaurants()`
+  - should return a collection of all the restaurants that the `Customer` has reviewed
+ 
+
+You can create a `debug.py` file and use it to check that these methods work before proceeding.
+For example, you should be able to call `session.query(Customer).first().restaurants` and see a list of the restaurants for the first customer in the database based on your seed data; and `session.query(Review).first().customer` should return the customer for the first review in the database.
+ 
+
+Aggregate and Relationship Methods
+Customer
+- `Customer full_name()`
+  - returns the full name of the customer, with the first name and the last name concatenated Western style.
+- `Customer favorite_restaurant()`
+  - returns the restaurant instance that has the highest star rating from this customer
+- `Customer add_review(restaurant, rating)`
+  - takes a `restaurant` (an instance of the `Restaurant` class) and a rating creates a new review for the restaurant with the given `restaurant_id`
+- `Customer delete_reviews(restaurant)`
+  - takes a `restaurant` (an instance of the `Restaurant` class) and removes **all** their reviews for this restaurant
+Review
+- `Review full_review()`
+  - should return a string formatted as follows:
+        - Review for {insert restaurant name} by {insert customer's full name}: {insert review star_rating} stars.
+Restaurant
+- `Restaurant fanciest(), this method should be a class method`
+  - returns _one_ restaurant instance for the restaurant that has the highest price
+- `Restaurant all_reviews()`
+  - should return a list of strings with all the reviews for this restaurant formatted as follows:
+
+ "Review for {insert restaurant name} by {insert customer's full name}: {insert review star_rating} stars.",
+  "Review for {insert restaurant name} by {insert customer's full name}: {insert review star_rating} stars.",
--- customer.py
+++ customer.py
@@ -20,15 +20,27 @@ class Customer(Base):
         self.lastName = lastName
         self.email = email
 
-class review(Base):
+
+class Review(Base):
+    __tablename__ ="reviews"
     reviewId = Column("reviewId", String,primary_key=True,default=generate_uuid)
-    customerId= Column("customerId", String, ForeignKey("customerId"))
+    customerId = Column("customerId", String, ForeignKey("customers.customerId"))
     rating = Column("rating",Integer)
 
-    def __init__(self,customerid,reviewid,rating):
-        self.customerid = customerid
-        self.reviewid = reviewid
+    def __init__(self,customerId,rating):
+        self.customerId = customerId
         self.rating = rating
+
+class Restaurant(Base):
+    __tablename__ = "restaurants"
+    restaurantID = Column("restaurantId", String, primary_key=True, default=generate_uuid)
+    name = Column("name", String)
+    price = Column("price", Integer)
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
 
 
 db = "sqlite:///ecomerceDB.db"
@@ -38,14 +50,141 @@ Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# create customer
-firstName = "Tobias"
-lastName = "oggg"
-email ="www@gmail.com"
 
-customers = Customer(firstName,lastName,email)
-session.add(customers)
+# Create a customer
+new_customer = Customer(firstName="Mary", lastName="Agatha", email="maryagat@gmail.com")
+new_customer = Customer(firstName="MORGAN", lastName="Mtua", email="morgan223@gmail.com")
+new_customer = Customer(firstName="Charline", lastName="Chege", email="chege494994@gmail.com")
+# new_customer = Customer(firstName="Tobias", lastName="Og", email="tobias330@gmail.com")
+session.add(new_customer)
+# session.add_all([])
 session.commit()
-print("customer added to database")
+print("Customer added to the database")
 
-# create review
+
+all_customers = session.query(Customer).all()
+for customer in all_customers:
+    print(f"Customer: {customer.firstName} {customer.lastName}, Email: {customer.email}")
+
+
+to_query = "some_customer_id"  
+customer = session.query(Customer).filter_by(customerID=to_query).first()
+if customer:
+    print(f"Customer: {customer.firstName} {customer.lastName}, Email: {customer.email}")
+else:
+    print("Customer not found")
+
+ # Update the email
+if customer:
+    customer.firstName ="elsy"
+    customer.email = "maryagatha@gmail.com" 
+    session.commit()
+    print("Customer updated")
+else:
+    print("Customer not found")
+
+#Delete the customer 
+if customer:
+    session.delete(customer)
+    session.commit()
+    print("Customer deleted")
+else:
+    print("Customer not found")
+
+
+# Create a new restaurant
+new_restaurant = Restaurant(name="The OG'S Chicken Taste", price=2500)
+new_restaurant = Restaurant(name="The Top Boys Burger", price=1900)
+new_restaurant = Restaurant(name="The London Derby Hot Piece", price=2300)
+new_restaurant = Restaurant(name= "The killer $ Hotter Pei", price=2000)
+
+session.add(new_restaurant)
+session.commit()
+print("Restaurant added to the database")
+
+# Query re...
+all_restaurants = session.query(Restaurant).all()
+for restaurant in all_restaurants:
+    print(f"Restaurant: {restaurant.name}, Price: {restaurant.price}")
+
+restaurant_id_to_update = "some_restaurant_id"
+restaurant = session.query(Restaurant).filter_by(restaurantID=restaurant_id_to_update).first()
+if restaurant:
+    restaurant.name = "old kings burger"
+    restaurant.price = 3000
+    session.commit()
+    print("Restaurant updated")
+else:
+    print("Restaurant not found")
+
+# Create a new review
+customer_id = "Tobias " 
+rating = 3 
+new_review = Review(customerId=customer_id, rating=rating)
+session.add(new_review)
+session.commit()
+print("Review added to the database")
+
+# Query all reviews
+all_reviews = session.query(Review).all()
+for review in all_reviews:
+    print(f"Review ID: {review.reviewId}, Customer ID: {review.customerId}, Rating: {review.rating}")
+
+customer_query = "Tobias"
+reviews_for_customer = session.query(Review).filter_by(customerId=customer_query).all()
+for review in reviews_for_customer:
+    print(f"Review ID: {review.reviewId}, Customer ID: {review.customerId}, Rating: {review.rating}")
+
+
+review_update = "Morgan"
+review = session.query(Review).filter_by(reviewId=review_update).first()
+if review:
+    review.rating = 4 
+    session.commit()
+    print("Review updated")
+else:
+    print("Review not found")
+
+
+review_delete = "Mary"
+review_to_delete = session.query(Review).filter_by(reviewId=review_delete).first()
+if review_to_delete:
+    session.delete(review_delete)
+    session.commit()
+    print("Review deleted")
+else:
+    print("Review not found")
+
+
+# # create customer
+# firstName = "Tobias"
+# lastName = "oggg"
+# email ="www@gmail.com"
+
+# customers = Customer(firstName,lastName,email)
+# session.add(customers)
+# session.commit()
+# print("customer added to database")
+
+# # create review
+# customer_id = customers.customerID
+# rating = 5 
+
+# Review = Review(customerId=customer_id, rating=rating)
+# session.add(Review)
+# session.commit()
+# print("Review added to the database")
+
+# # Create restaurant
+# restaurant_name = "king bugers"
+# restaurant_price = 3400
+
+# restaurant = Restaurant(name=restaurant_name, price=restaurant_price)
+# session.add(restaurant)
+# session.commit()
+# print("Restaurant added to the database")
+
+
+
+
+
